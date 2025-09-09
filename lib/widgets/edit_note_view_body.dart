b2b6300a2fdfa8bbe540f80ba16627efdd4f3fb2
NOUR-MOWAFEY <nourmowafey82@gmail.com>
2025-09-09 22:26:45 +0300

    add custom text field to edit note view

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -10,7 +11,15 @@ class EditNoteViewBody extends StatelessWidget {
       child: Scaffold(
         body: Padding(
           padding: EdgeInsets.only(top: 24, right: 24, left: 24),
-          child: Column(children: [CustomAppBar(title: 'Edit Note', icon: Icons.check,)]),
+          child: Column(
+            children: [
+              CustomAppBar(title: 'Edit Note', icon: Icons.check),
+              SizedBox(height: 40),
+              CustomTextField(hintText: 'title'),
+              SizedBox(height: 16),
+              CustomTextField(hintText: 'content', maxLines: 5),
+            ],
+          ),
         ),
       ),
     );
